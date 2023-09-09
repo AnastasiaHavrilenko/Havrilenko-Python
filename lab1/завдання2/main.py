b30e24a6d9587d229356f3c8491a0de86639da45
Анастасія Гавріленко <144366266+AnastasiaHavrilenko@users.noreply.github.com>
2023-09-10 01:08:46 +0300

    Add files via upload

--- lab1/завдання2/main.py
+++ lab1/завдання2/main.py
@@ -1,1 +1,10 @@
-
+f1 = 0
+f2 = 1
+print ("Перші 8 чисел з ряду Фібоначчі: ")
+print (f1, f2, end = ' ')
+sum = f1 + f2
+for i in range(2, 8):
+  f1, f2 = f2, f1 + f2 
+  print (f2, end = ' ')
+  sum = sum + f2
+print ("\nЇх сума: ", sum)
