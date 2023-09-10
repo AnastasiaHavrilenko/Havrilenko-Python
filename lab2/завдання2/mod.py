60eb0f0ced698ba570ed6e2741a9ee7b2d7e664a
Анастасія Гавріленко <144366266+AnastasiaHavrilenko@users.noreply.github.com>
2023-09-10 13:46:24 +0300

    Update mod.py

--- lab2/завдання2/mod.py
+++ lab2/завдання2/mod.py
@@ -1,1 +1,10 @@
-
+def shlyah (n):    #mod.py
+  if n == 1 :
+    result = 10
+  else:
+    result = 10
+    probig = 10
+    for i in range (1,n,1):
+      probig *=  1.1
+      result += probig
+  return result
