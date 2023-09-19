1a4113b61f54cafb97fcc540bd8ef0ee4bca666e
Анастасія Гавріленко <144366266+AnastasiaHavrilenko@users.noreply.github.com>
2023-09-19 23:54:57 +0300

    Update main.py

--- lab3/завдання2/main.py
+++ lab3/завдання2/main.py
@@ -1,1 +1,23 @@
+def povtoru(slovo):
+    literu = {}
+    spysok = []
 
+    for l in slovo:
+        if l in literu:
+            literu[l] += 1
+        else:
+            literu[l] = 1
+
+    for l, kilkist in literu.items():
+        if kilkist >= 2:
+            spysok.append(l)
+
+    return spysok
+
+slovo = input("Введіть слово: ")
+result = povtoru(slovo)
+
+if result:
+    print("Повторювані літери в слові:",", ".join(result))
+else:
+    print("У слові немає повторюваних літер.")
