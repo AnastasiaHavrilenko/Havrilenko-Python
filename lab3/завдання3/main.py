e5ef452f5de4cdcc619d12bc4a5b45944c862cbe
Анастасія Гавріленко <144366266+AnastasiaHavrilenko@users.noreply.github.com>
2023-09-20 00:29:19 +0300

    Update main.py

--- lab3/завдання3/main.py
+++ lab3/завдання3/main.py
@@ -1,1 +1,14 @@
+def slova_n(rechennya):
+    slova = rechennya.split()  # Розбиваємо речення на слова
+    litera_n = []
 
+    for s in slova:
+        if s.startswith('н') or s.startswith('Н'):  # Перевіряємо на початкову літеру 'н' або 'Н'
+            litera_n.append(s)
+
+    return len(litera_n)
+
+rechennya = input("Введіть речення: ")
+result = slova_n(rechennya) 
+
+print("Кількість слів, що починаються з 'н':", result)
