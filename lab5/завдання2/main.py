8be7b264f8f41fc23d51faa0043c5358a676baa4
Анастасія Гавріленко <144366266+AnastasiaHavrilenko@users.noreply.github.com>
2023-09-27 23:59:48 +0300

    Update main.py

--- lab5/завдання2/main.py
+++ lab5/завдання2/main.py
@@ -1,1 +1,11 @@
+n = 7
+array = [[0] * n for _ in range(n)]
 
+# Заповнення масиву
+for i in range(n):
+    for j in range(i+1):
+        array[j-i+6][i] = j+1
+
+# Вивід заповненого массива:
+for row in array:
+    print(" ".join(map(str, row)))
