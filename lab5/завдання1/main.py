f23b502bfc064f1ec7017863cce2393130b5990c
Анастасія Гавріленко <144366266+AnastasiaHavrilenko@users.noreply.github.com>
2023-09-28 00:12:10 +0300

    Update main.py

--- lab5/завдання1/main.py
+++ lab5/завдання1/main.py
@@ -1,1 +1,16 @@
+x = int(input("Введіть кількість елементів масива х = "))
 
+print(f"Введіть {x} елементів масиву:")
+
+arr = [int(input()) for i in range(x)]
+sum = 0
+kilkist = 0
+for t in arr:
+  if t < 0:
+    sum += t
+    kilkist += 1
+if kilkist == 0:
+  print("В масиві немає від'ємних чисел")
+else:
+  result = sum / kilkist
+  print("Середнє арифметичне від'ємних чисел:", result)
