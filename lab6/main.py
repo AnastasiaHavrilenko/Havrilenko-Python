cf52ab7d1dbe22da0c647fc757004698272ea2d3
Анастасія Гавріленко <144366266+AnastasiaHavrilenko@users.noreply.github.com>
2023-10-03 12:39:43 +0300

    Update main.py

--- lab6/main.py
+++ lab6/main.py
@@ -1,1 +1,15 @@
+def add_element():
 
+    A = list(map(int,input('Введіть список елементів: ').split()))
+
+    print(A)
+
+    position = int(input("Введіть позицію для вставки: "))
+    while position < 0 or position > len(A):
+       print("Помилка: неправильна позиція")
+       position = int(input("Введіть позицію для вставки: "))    
+    element = int(input("Введіть елемент для вставки: "))
+    A.insert(position, element)
+    print(A)
+  
+add_element()
